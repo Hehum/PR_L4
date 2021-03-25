b0a12c11f4c0dea0077be046a24db0fd1cc49299
Hehum <jarekkotowicz1@gmail.com>
2021-03-25 20:30:52 +0100

    quiz

--- js/script.js
+++ js/script.js
@@ -171,7 +171,30 @@ let preQuestions =
             ]
         }];
 
+fetch('https://quiztai.herokuapp.com/api/quiz')
+    .then(resp=>resp.json())
+    .then(resp => {
+    preQuestions=resp;
+    question.innerHTML = preQuestions[index].question;
+if (preQuestions[index].answers.length === 2) {
+       answers[2].style.display = 'none';
+       answers[3].style.display = 'none';
+   } else {
+       answers[2].style.display = 'block';
+       answers[3].style.display = 'block';
+   }
+
+   answers[0].innerHTML = preQuestions[index].answers[0];
+   answers[1].innerHTML = preQuestions[index].answers[1];
+   answers[2].innerHTML = preQuestions[index].answers[2];
+   answers[3].innerHTML = preQuestions[index].answers[3];
+    
+});
+
+
+
 let next = document.querySelector('.next');
+let previous = document.querySelector('.previous');
 
 let question = document.querySelector('.question');
 let answers = document.querySelectorAll('.list-group-item');
@@ -181,28 +204,103 @@ let restart = document.querySelector('.restart');
 let index = 0;
 let points = 0;
 
-for (let i = 0; i < answers.length; i++) {
-    answers[i].addEventListener('click', doAction);
+
+
+function setQuestion(index) {
+   //clearClass();
+   question.innerHTML = preQuestions[index].question;
+if (preQuestions[index].answers.length === 2) {
+       answers[2].style.display = 'none';
+       answers[3].style.display = 'none';
+   } else {
+       answers[2].style.display = 'block';
+       answers[3].style.display = 'block';
+   }
+
+   answers[0].innerHTML = preQuestions[index].answers[0];
+   answers[1].innerHTML = preQuestions[index].answers[1];
+   answers[2].innerHTML = preQuestions[index].answers[2];
+   answers[3].innerHTML = preQuestions[index].answers[3];
 }
 
+
+
+
+
+next.addEventListener('click', function () {
+   index++;
+   if (index >= preQuestions.length) {
+       list.style.display = 'none';
+       results.style.display = 'block';
+       userScorePoint.innerHTML = points;
+   } else {
+       setQuestion(index);
+       activateAnswers();
+   }
+   
+});
+
+
+
+
+
+previous.addEventListener('click',function(){
+    index--;
+    if(index<0){
+        index=0;
+        return;
+    }
+    setQuestion(index);
+})
+function activateAnswers() {
+   for (let i = 0; i < answers.length; i++) {
+      answers[i].addEventListener('click', doAction);
+   }
+}
+//activateAnswers();
+
+
+
 function doAction(event) {
-    //event.target - Zwraca referencję do elementu, do którego zdarzenie zostało pierwotnie wysłane.
-    if (event.target.innerHTML === preQuestions[index].correct_answer) {
-        points++;
-        pointsElem.innerText = points;
-        markCorrect(event.target);
-    }
-    else {
-        markInCorrect(event.target);
-    }
-    disableAnswers();
+   
+   //event.target - Zwraca referencję do elementu, do którego zdarzenie zostało pierwotnie wysłane.
+   if (event.target.innerHTML === preQuestions[index].correct_answer) {
+       points++;
+       pointsElem.innerText = points;
+      
+       markCorrect(event.target);
+   }
+   else {
+       
+       markInCorrect(event.target);
+   }
+   disableAnswers();
+    
+}
+
+
+function markCorrect(elem) {
+   elem.classList.add('correct');
+    
+}
+function markInCorrect(elem) {
+   elem.classList.add('incorrect');
+}
+function disableAnswers() {
+   for (let i = 0; i < answers.length; i++) {
+      answers[i].removeEventListener('click', doAction);
+   }
+}
+function unmark(elem){
+    elem.classList.remove('correct');
+    elem.classList.remove('incorrect');
 }
 
 
 
 restart.addEventListener('click', function (event) {
     event.preventDefault();
-
+    
     index = 0;
     points = 0;
     let userScorePoint = document.querySelector('.score');
